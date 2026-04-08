997cc9fd253b44115e4f0cc344dd98273b7d115e
Marinella Lanza <lanza_marinella@libero.it>
2026-04-08 19:14:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/beauty-landing-react/",
   plugins: [react(), tailwindcss()],
-  base: '/beauty-landing-react/',
 });
